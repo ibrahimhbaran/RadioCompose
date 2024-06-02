84d93b25c39e852b14ab01bbe531f21fb73d9aaa
ibrahimhbaran <baranibrahimh@gmail.com>
2024-06-02 21:22:01 +0200

    Created API module within DI package for Hilt support Create BASE URL for end points

---
+++ app/src/main/java/com/example/radiotest/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.radiotest
+
+const val BASE_URL = "https://fakestoreapi.com/"
---
+++ app/src/main/java/com/example/radiotest/RadioApp.kt
@@ -0,0 +1,7 @@
+package com.example.radiotest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RadioApp : Application()
