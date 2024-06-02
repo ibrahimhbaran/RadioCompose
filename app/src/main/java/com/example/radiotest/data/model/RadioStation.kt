64b1ee1c25bea483b59e44e82a22fd01007b44fa
ibrahimhbaran <baranibrahimh@gmail.com>
2024-06-02 21:05:22 +0200

    Created RadioService.kt for Retrofit client

--- app/src/main/java/com/example/radiotest/data/model/RadioStation.kt
+++ app/src/main/java/com/example/radiotest/data/model/RadioStation.kt
@@ -2,7 +2,7 @@ package com.example.radiotest.data.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Radio(
+data class RadioStation(
     @SerializedName("changeuuid") val changeuuid: String? = null,
     @SerializedName("stationuuid") val stationuuid: String? = null,
     @SerializedName("serveruuid") val serveruuid: String? = null,
